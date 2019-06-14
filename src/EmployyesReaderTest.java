54c283ada1c2d6648e5ab87cf292c89cf26ed420
Artur Przyjemski <spinezpl@gmail.com>
2019-06-14 09:45:31 +0200

    second commit with file companyStatistics.txt

--- src/EmployyesReaderTest.java
+++ src/EmployyesReaderTest.java
@@ -1,25 +1,42 @@
 import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class EmployyesReaderTest {
     public static void main(String[] args) throws FileNotFoundException {
         EmployeeReader employeeReader = new EmployeeReader();
+        String fileName = "companyStatistic.txt";
 
 
-        try {
+        try(
+                var fileWriter = new FileWriter(fileName);
+                var writer = new BufferedWriter(fileWriter);
+                ) {
             Employyes[] employees = employeeReader.readEmployyes("Employees.csv");
             EmployeeStatistics stat1 = new EmployeeStatistics();
-
-            System.out.println(stat1.avgSalary(employees));
-            System.out.println(stat1.minSalary(employees));
-            System.out.println(stat1.maxSalary(employees));
-            System.out.println(stat1.employeesQuantity(employees));
-            System.out.println(stat1.employeesInDepartament(employees,"it"));
-            System.out.println(stat1.employeesInDepartament(employees,"Managment"));
-            System.out.println(stat1.employeesInDepartament(employees,"Support"));
+            writer.write("Srednia wypłata :" +stat1.avgSalary(employees) );
+            writer.newLine();
+            writer.write("Najmniejsza wypłata :" + stat1.minSalary(employees));
+            writer.newLine();
+            writer.write("Największa wypłata :" +stat1.maxSalary(employees) );
+            writer.newLine();
+            writer.write("Łączna liczba pracowników :" + stat1.employeesQuantity(employees));
+            writer.newLine();
+            writer.write("Liczba pracowników w dziale IT :" + stat1.employeesInDepartament(employees,"it"));
+            writer.newLine();
+            writer.write("Liczba pracowników w dziale Managment :" + stat1.employeesInDepartament(employees,"Managment"));
+            writer.newLine();
+            writer.write("Liczba pracowników w dziale Support :" + stat1.employeesInDepartament(employees,"Support"));
+            writer.newLine();
 
         } catch (FileNotFoundException e) {
             System.out.println("Nie ma takiego pliku");
 
+
+        }
+        catch (IOException e) {
+            System.out.println("Nie udało się zapisać pliku" + fileName);
         }
 
     }
